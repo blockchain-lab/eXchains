33d4b7fed94209f24c0305defad6712e856c488d
Michal Loin <michal.loin@gmail.com>
2017-12-19 15:57:54 +0100

    Clean up on client

--- tendermint/client/client.py
+++ tendermint/client/client.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-
-import sys
 import ed25519 as ecdsa
 import time
 import uuid
@@ -11,108 +9,71 @@ import base64
 import json
 
 class Client:
-	def __init__(self, address='localhost', port=46657):
+	def __init__(self, address='localhost', port=46657, data_file='data.csv', time_interval=900):
 		self.uuid = uuid.uuid4()
 		# optional storage of the uuid
 		# open('uuid', 'wb').write(self.uuid.bytes)
 
 		self.address = address
 		self.port = port
+		self.data_file = data_file
+		self.time_interval = time_interval
+		
 		self.priv_key, self.public_key = ecdsa.create_keypair()
-
 		# optional storage of keys
 		#open("private_key","wb").write(self.priv_key.to_bytes())
 
-		with open('data.csv', 'r') as data:
+		with open(self.data_file, 'r') as data:
 			self.data = data.readlines()
 
 	def register_to_abci(self):
-		#send public key and UUID
-		pass
+		msg = tx.Transaction()
+		msg.new_contract.uuid = self.uuid.bytes
+		msg.new_contract.timestamp = int(time.time())
+		msg.new_contract.public_key = self.public_key.to_bytes()
+		payload = self.uuid.bytes + \
+				  msg.new_contract.timestamp.to_bytes(8, byteorder='big') + \
+				  self.public_key.to_bytes()
+		msg.new_contract.signature = self.priv_key.sign(payload)
+		data = msg.SerializeToString()
 
+		self.sendRequest(data)
 
 	def sendRequest(self, binarystring):
-
-
 		url = 'http://{}:{}/'.format(self.address, self.port)  # Set destination URL here
-		post_fields = {'foo': 'bar'}  # Set POST fields here
-
+		
 		request = Request(url, json.dumps({
 			"method": 'broadcast_tx_sync',
 			"params": [base64.b64encode(binarystring).decode('ascii')],
 			"jsonrpc": "2.0",
 			"id": "not_important"
 		}).encode())
+		
 		result = urlopen(request).read().decode()
 		print(result)
 
 	def run(self):
+		self.register_to_abci()
 		counter = 1
-		msg = tx.Transaction()
-		msg.new_contract.uuid = self.uuid.bytes
-		msg.new_contract.timestamp = int(time.time())
-		msg.new_contract.public_key = self.public_key.to_bytes()
-		payload = self.uuid.bytes + bytes(msg.new_contract.timestamp) + self.public_key.to_bytes()
-		msg.new_contract.signature = self.priv_key.sign(payload)
-
-		data = msg.SerializeToString()
-
-		print(base64.urlsafe_b64encode(data).decode('ascii'), data)
-		# request.urlopen()
-		self.sendRequest(data)
-		# with request.urlopen('http://{}:{}/broadcast_tx_async?tx="{}"'.format(self.address, self.port, base64.urlsafe_b64encode(data).decode('ascii'))) as response:
-		# 	print(response.read(300))
-
 		try:
 			while True:
-
-				# msg = {}
-				# msg['UUID'] = str(self.uuid)
-				# msg['Timestamp'] = int(time.time())
-				# msg['Consumption'] = self.data[counter].split(';')[3]
-				# msg['Production'] = self.data[counter].split(';')[4]
-				#
-				# # kinda ugly way to generate stuff to sign
-				# payload = bytes(str(msg['UUID']) + str(msg['Timestamp']) + str(msg['Consumption']) + str(msg['Production']), encoding='UTF-8')
-				# msg['Signature'] = self.priv_key.sign(payload, encoding='base64').decode('ascii')
-				# #msg_json = json.dumps(msg)
-				# #msg_json = json.dumps('*'.join((str(self.uuid), str(int(time.time())), self.data[counter].split(';')[3], self.data[counter].split(';')[4])))
-				# #print(msg_json, json.dumps("chuj"))
-				#
-				#
-				# myurl = "http://{}:{}/broadcast_tx_async".format(self.address, self.port)
-				# req = request.Request(myurl)
-				# req.add_header('Content-Type', 'application/json; charset=utf-8')
-				# jsondata = json.dumps(msg)
-				# jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-				# req.add_header('Content-Length', len(jsondataasbytes))
-				# print (jsondataasbytes)
-
-				#with request.urlopen(req, jsondataasbytes) as response:
-				#	print(response.read(300))
-
 				msg = tx.Transaction()
 				msg.usage.contract_uuid = self.uuid.bytes
 				msg.usage.timestamp = int(time.time())
 				msg.usage.consumption = int(float(self.data[counter].split(';')[3].replace(',', '.')) * pow(10, 7))
 				msg.usage.production = int(float(self.data[counter].split(';')[4].replace(',', '.')) * pow(10, 7))
-				payload = self.uuid.bytes + bytes(msg.usage.timestamp) + bytes(msg.usage.consumption) + bytes(msg.usage.production)
+				payload = self.uuid.bytes + \
+						  msg.usage.timestamp.to_bytes(8, byteorder='big') + \
+						  msg.usage.consumption.to_bytes(8, byteorder='big') + \
+						  msg.usage.production.to_bytes(8, byteorder='big')
 				msg.usage.signature = self.priv_key.sign(payload)
-
 				data = msg.SerializeToString()
-
 				self.sendRequest(data)
-				# sending, must be changed
-				# with request.urlopen('http://{}:{}/broadcast_tx_async?tx="{}"'.format(self.address, self.port, )) as response:
-				# 	print(response.read(300))
 				
-				# signature verification, should be on server side only
-				#try:
-				#	self.public_key.verify(msg['Signature'], payload, encoding="base64")
-				#except ecdsa.BadSignatureError as e:
-				#	print('NOPE')
-
-				input()
+				if self.time_interval == 0:
+					input()
+				else:
+					time.sleep(self.time_interval)
 				counter += 1
 		except KeyboardInterrupt:
 			print('\nExiting\n')
