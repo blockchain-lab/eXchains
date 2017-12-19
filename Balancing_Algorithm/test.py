ef996130c0de366f7edea3e066d169395688cee5
Niels Hokke <nielshokke@gmail.com>
2017-12-19 13:23:05 +0100

    simulation usage in python

--- Balancing_Algorithm/test.py
+++ Balancing_Algorithm/test.py
@@ -1,4 +1,42 @@
 import BalancingAlgorithm as Balancing
+import CSVparser
+import csv
+import time
+from array import *
 
-Algorithm = Balancing.Algorithm()
-Algorithm.print_hello()
+
+numClients = 5
+clientOffset = 1440
+dayOffset = 720
+
+minPerBlock = 5
+secPerBlock = 2
+powSignificance = 1000
+powUnit = "Kwh"
+
+
+parser = CSVparser.CVSparer('SimulationData.csv', numClients)
+
+for i in range(0, numClients):
+    parser.skipRows(i, dayOffset)
+    parser.skipRows(i, i*clientOffset)
+
+consumptionSum = 0
+productionSum = 0
+
+while True:
+    for clientID in range(0, numClients):
+        for min in range(0, minPerBlock):
+            row = parser.getNextRow(clientID)
+            consumptionSum += int(float(row[3].replace(",", "."))*powSignificance)
+            productionSum += int(float(row[4].replace(",", "."))*powSignificance)
+
+        print("Client {}: consumptionSum: {} {}, productionSum: {} {}".format(clientID, consumptionSum, powUnit, productionSum, powUnit))
+        consumptionSum = 0
+        productionSum = 0
+    print("\n")
+    time.sleep(secPerBlock)
+
+
+
+
