55ca1d60786947d9c8386d593cdabcc223cc88be
jetsebrouwer <jetse.brouwer@gmail.com>
2017-12-19 23:57:34 +0100

    Implemented a very basic matching algorithm. Todo: Pro rata and deterministic splitting of residual quantities

--- Balancing_Algorithm/MatchMaker.py
+++ Balancing_Algorithm/MatchMaker.py
@@ -1,3 +1,6 @@
+from enum import Enum
+import operator
+
 class OrderBook:
     askList = []
     bidList = []
@@ -11,23 +14,29 @@ class OrderBook:
             self.bidList.append(order)
         elif isinstance(order, list):
             for orders in order[:]:
-                print("Oooeh addind a thing from the list")
+                print("Oooeh adding a thing from the list")
                 self.addorder(orders)
 
     def getbidlist(self):
-        return self.askList
-
-    def getaskist(self):
         return self.bidList
 
+    def getasklist(self):
+        return self.askList
 
 class Ask:
-    uuid: int
-    orderId: int
-    volume: int
-    price: int
-    Asks = []
-    Bids = []
+    uuid = 0
+    orderId = 0
+    volume = 0
+    price = 0
+
+    def __init__(self, uuid, orderid, volume, price):
+        self.uuid = uuid
+        self.orderId = orderid
+        self.volume = volume
+        self.price = price
+
+    def __repr__(self):
+        return "uuid: {}, orderid: {}, volume: {}, price: {}".format(self.uuid, self.orderId, self.volume, self.price)
 
 
 class Bid:
@@ -43,7 +52,7 @@ class Bid:
         self.price = price
 
     def __repr__(self):
-        return "\nuuid: {}, orderid: {}, volume: {}, price: {}".format(self.uuid, self.orderId, self.volume, self.price)
+        return "uuid: {}, orderid: {}, volume: {}, price: {}".format(self.uuid, self.orderId, self.volume, self.price)
 
 
 class Bid:
@@ -59,7 +68,8 @@ class Bid:
         self.price = price
 
     def __repr__(self):
-        return "\nuuid: {}, orderid: {}, volume: {}, price: {}".format(self.uuid, self.orderId, self.volume, self.price)
+        return "uuid: {}, orderid: {}, volume: {}, price: {}".format(self.uuid, self.orderId, self.volume, self.price)
+
 
 class Trades:
     transactions = []
@@ -72,10 +82,49 @@ class Trades:
             print("ID: {}, Type: {}, Energy: {}, Price: {}".format(transaction.ID, transaction.type, transaction.energy, transaction.price))
 
 
-class Transaction:
+class OrderType(Enum):
+    ASK = 1
+    BID = 2
 
-    def __init__(self, clientID, type, energy, price):
-        self.ID = clientID
-        self.type = type
-        self.energy = energy
+
+class Transaction:
+    def __init__(self, uuid, orderid, ordertype: OrderType, volume, price):
+        self.ID = uuid
+        self.orderId = orderid
+        self.orderType = ordertype
+        self.volume = volume
         self.price = price
+
+    def __repr__(self):
+        return "ID: {}, Type: {}, Energy: {}, Price: {}".format(self.ID, self.orderId,self.orderType, self.volume,self.price)
+
+class Matcher:
+    def match(self,orderbook: OrderBook):
+        askList = sorted(orderbook.getasklist(), key=operator.attrgetter('price', 'volume'), reverse=True)
+        bidList = sorted(orderbook.getbidlist(), key=operator.attrgetter('price', 'volume'), reverse=False)
+
+
+
+        previousprice = 0;
+        while len(askList) != 0 and len(bidList) != 0 and askList[0].price > bidList[0].price:
+            # The top orders orders for a certain price point will always be full filled
+            # This is to encourage offering at prices that are good for the market
+            if (previousprice!=askList[0].price and previousprice != bidList[0].price):
+                volume = min(askList[0].volume, bidList[0].volume)
+                print("New matching round, price will be: {}".format((askList[0].price+bidList[0].price)/2))
+                print(bidList[0])
+                print(askList[0])
+                if askList[0].volume == volume:
+                    askList.pop(0)
+                else:
+                    askList[0].volume -= volume
+                    print(askList[0])
+
+                if bidList[0].volume == volume:
+                    askList.pop(0)
+                else:
+                    bidList[0].volume -= volume
+                    print(bidList[0])
+
+
+        print("No matches can be made anymore")
--- Balancing_Algorithm/test.py
+++ Balancing_Algorithm/test.py
@@ -1,17 +1,27 @@
 import MatchMaker
+import operator
 
 book = MatchMaker.OrderBook()
-koop = MatchMaker.Ask(0,0,12,34)
-koop1 = MatchMaker.Ask(0,0,12,34)
-verkoop = MatchMaker.Bid(0,1,43,21)
-kooplijst = [koop, verkoop, koop]
+koop = MatchMaker.Ask(0,0,20,34)
+koop1 = MatchMaker.Ask(0,0,50,32)
+koop2 = MatchMaker.Ask(0,0,40,35)
+koop3 = MatchMaker.Ask(0,0,45,35)
+
+
+verkoop = MatchMaker.Bid(0,1,32,29)
+verkoop1 = MatchMaker.Bid(0,1,47,31)
+verkoop2 = MatchMaker.Bid(0,1,43,33)
+verkoop3 = MatchMaker.Bid(0,1,33,31)
+
+
+kooplijst = [koop, koop1, koop2, koop3, verkoop, verkoop1, verkoop2, verkoop3]
 
 book.addorder(kooplijst)
 
-asklist = book.getbidlist
+engine = MatchMaker.Matcher()
+engine.match(book)
 
-#print(asklist)
-print(kooplijst)
+# print(asklist.sort(key=MatchMaker.Ask.price))
 
 
 
