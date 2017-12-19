aa42120ccb0c3acef79c6944b3d9089a5d58df46
Niels Hokke <nielshokke@gmail.com>
2017-12-19 16:25:15 +0100

    added trades object

--- Balancing_Algorithm/CSVparser.py
+++ Balancing_Algorithm/CSVparser.py
@@ -1,4 +1,4 @@
-class CVSparer(object):
+class CVSparer:
 
     clientPointers = []
 
--- Balancing_Algorithm/MatchMaker.py
+++ Balancing_Algorithm/MatchMaker.py
@@ -1,14 +1,12 @@
-class OrderBook():
+class OrderBook:
     Asks = []
     Bids = []
-
-    
 
 
 class Bid:
     uuid = 0
     orderId = 0
-    volume =0
+    volume = 0
     price = 0
 
     def __init__(self, uuid, orderid, volume, price):
@@ -28,4 +26,24 @@ class Ask:
         self.uuid = uuid
         self.orderId = orderid
         self.volume = volume
+        self.price = price
+
+
+class Trades:
+    transactions = []
+
+    def addtransaction(self, transaction):
+        self.transactions.append(transaction)
+
+    def printtrades(self):
+        for transaction in self.transactions:
+            print("ID: {}, Type: {}, Energy: {}, Price: {}".format(transaction.ID, transaction.type, transaction.energy, transaction.price))
+
+
+class Transaction:
+
+    def __init__(self, clientID, type, energy, price):
+        self.ID = clientID
+        self.type = type
+        self.energy = energy
         self.price = price
